722b5b8034a0db4acfbebe08a96dc6d97c24b8e0
Falcon <atalikfurkan@gmail.com>
2023-03-14 16:52:02 +0000

    Initial Commit

---
+++ japon/japonApp.swift
@@ -0,0 +1,17 @@
+//
+//  japonApp.swift
+//  japon
+//
+//  Created by WildFalcon on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct japonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
